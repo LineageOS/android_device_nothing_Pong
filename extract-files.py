00871843824fe727b0e9013d66ff78a2eb356f6b
Arian <arian.kulmer@web.de>
2025-02-18 18:03:24 +0100

    Pong: Set qcril persist.vendor.radio.poweron_opt to 0
    Change-Id: Ie392219d8121edf05270a9b66f2032f3202e9efe

--- extract-files.py
+++ extract-files.py
@@ -89,8 +89,6 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libutils.so'),
     'vendor/lib64/libqcodec2_core.so': blob_fixup()
         .add_needed('libcodec2_shim.so'),
-    ('vendor/lib64/libqcrilNr.so', 'vendor/lib64/libril-db.so'): blob_fixup()
-        .binary_regex_replace(rb'persist\.vendor\.radio\.poweron_opt', rb'persist.vendor.radio.poweron_ign'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
